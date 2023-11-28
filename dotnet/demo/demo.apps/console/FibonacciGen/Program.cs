a000cc5bf377053c146d9a4dcc53dd9206f5e1ef
homoregie <homoregie@hotmail.com>
2023-11-27 20:17:06 -0600

    Add base instructor and student project files

---
+++ dotnet/demo/demo.apps/console/FibonacciGen/Program.cs
@@ -0,0 +1,23 @@
+﻿using FibonacciGen.Services;
+using System.Runtime.CompilerServices;
+
+//Display character to separate header and output body
+void DisplaySectionSeperator()
+{
+    Console.WriteLine(new string('=', 40));
+}
+
+//Create instance of fibonacci generator 
+var generator = new FibonacciGenerator();
+
+//Generate 15 numbers
+Console.WriteLine("Output of 15 fibonacci numbers");
+DisplaySectionSeperator();
+foreach (var digit in generator.Generate(30))
+{
+    Console.WriteLine(digit);
+}
+
+DisplaySectionSeperator();
+Console.WriteLine("Program completed successfully");
+
